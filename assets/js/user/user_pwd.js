8d087416fda84a56b05209d60478c916439c60dd
lzk <18075284063@163.com>
2020-07-20 13:54:09 +0800

    7.20中午提交

--- assets/js/user/user_pwd.js
+++ assets/js/user/user_pwd.js
@@ -15,4 +15,21 @@ $(function() {
             }
         }
     })
+
+    $('.layui-form').on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/my/updatepwd',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status != 0) {
+                    return layui.layer.msg('更新失败');
+                }
+                layui.layer.msg('更新成功');
+                $('.layui-form')[0].reset();
+            }
+
+        })
+    })
 })
